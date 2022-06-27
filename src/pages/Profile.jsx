6cbfd464b3d730709c6f36095ea3c0a8ccff865d
jojothomas1515 <jojothomas1515@gmail.com>
2022-06-27 17:18:27 +0100

    profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,34 +1,57 @@
 import React, {useContext, useEffect, useRef, useState} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faClose, faFireExtinguisher, faUserEdit} from "@fortawesome/free-solid-svg-icons";
+import {faClose, faImage, faUserEdit} from "@fortawesome/free-solid-svg-icons";
 
 import '../css/pages/profile.css';
 import {fetchUserInfo} from "../utilities/userInfo";
 import {AuthContext} from "../utilities/AuthContext";
+
 function Profile(props) {
     const {accessToken} = useContext(AuthContext)
     const profileImageModalRef = useRef()
+    const editProfileModalRef = useRef()
+    const pimRef = useRef()
     const [profileInfo, setProfileInfo] = useState("")
 
-    useEffect(()=>{
-        fetchUserInfo(accessToken, setProfileInfo)
-        console.log(profileInfo)
+    useEffect(() => {
+            fetchUserInfo(accessToken, setProfileInfo)
+            console.log(profileInfo)
         },
-       [])
+        [])
 
-    function openProfileImage(){
+    function openProfileImage() {
         profileImageModalRef.current.style.display = 'flex'
     }
-    function closeProfileImage(){
+
+    function closeProfileImage() {
         profileImageModalRef.current.style.display = 'none'
 
+    }
+    function closeEditProfile(){
+        editProfileModalRef.current.style.display='none'
+    }
+
+    function openEditProfile() {
+        editProfileModalRef.current.style.display = 'flex'
+    }
+
+
+    function changeImage(e) {
+        const file = new FileReader()
+
+        file.onload = (ev) => {
+            pimRef.current.src = ev.target.result
+        }
+        file.readAsDataURL(e.target.files[0]
+        )
     }
 
 
     return (
         <>
-            <div className="contain profile">
-                <FontAwesomeIcon className={'btn edit-profile'} data-edit-info icon={faUserEdit} onClick={() => console.log('modal opener')}/>
+            <section className="contain profile">
+                <FontAwesomeIcon className={'btn edit-profile'}  icon={faUserEdit}
+                                 onClick={openEditProfile}/>
                 <div>
                     <img src={require('../images/pages/index.jpeg')} alt="profile image" onClick={openProfileImage}/>
                 </div>
@@ -38,14 +61,46 @@ function Profile(props) {
                     <p>{profileInfo.email}</p>
 
                 </div>
-            </div>
+            </section>
 
-            <div className={'profile-image-modal'} ref={profileImageModalRef}>
+            <section className={'profile-image-modal'} ref={profileImageModalRef}>
                 <FontAwesomeIcon icon={faClose} className={'btn profile-image-modal-icon'} onClick={closeProfileImage}/>
                 <div className={'img-con'}>
                     <img src={require('../images/pages/cool.jpg')}/>
                 </div>
-            </div>
+            </section>
+
+            <section className={'edit-profile-modal'} ref={editProfileModalRef}>
+
+                <FontAwesomeIcon icon={faClose} onClick={closeEditProfile} className={'btn btn-danger close-edit'}/>
+
+                <div className="edit-profile-con">
+                    <div className={'pim-con'} style={{position: "relative"}}>
+                        <img src={require('../images/pages/index.jpeg')} alt="profile image" ref={pimRef}
+                        />
+                        <label htmlFor={'selectProfileImage'} style={{cursor: "pointer"}}><FontAwesomeIcon
+                            icon={faImage}/></label>
+                        <input type={'file'} accept={'image/*'} id="selectProfileImage" style={{display: "none"}}
+                               onChange={changeImage}/>
+
+                    </div>
+                    <form>
+
+                        <div className={"form-con"}>
+                            <div className={'form-control'}><input type={"text"} placeholder={'First Name'}
+                                                                   value={profileInfo.firstname}/>
+                            </div>
+                            <div className={'form-control'}>
+                                <input type={"text"} placeholder={'Last Name'} value={profileInfo.lastname}/>
+                            </div>
+                            <div><input className={'btn'} type={'submit'} value={'Update Info'}/></div>
+
+                        </div>
+                    </form>
+
+                </div>
+
+            </section>
 
         </>
     );
